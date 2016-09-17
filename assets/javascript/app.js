9255af2d37e481dc71697998dc43e229b924d8fb
jj99999 <jonathan.jackson3@gmail.com>
2016-09-17 11:18:12 -0400

    updated html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,3 +6,32 @@ var config = {
    messagingSenderId: "1059478718374"
  };
  firebase.initializeApp(config);
+
+// Variable to reference the database
+var database = firebase.database();
+
+
+// Submit Button Click
+$("#submitbtn").on("click", function() {
+
+	// Code in the logic for storing and retrieving the most recent employee.
+	var name = $("#employee-name").val().trim();
+	var startDate = $("#start-date").val().trim();
+	var role = $("#employee-role").val().trim();
+	var rate = $("#monthly-rate").val().trim();
+
+	var employee = {
+		name: name,
+		startDate: startDate,
+		role: role,
+		rate: rate
+	}
+
+	console.log(JSON.stringify(employee));
+	
+    // Save new value to Firebase
+	database.ref().push(employee);
+
+	// Don't refresh the page!
+	return false;
+});
