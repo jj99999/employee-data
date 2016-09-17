5523b28681ec94c7887048bca8fe47869eefa8f7
jj99999 <jonathan.jackson3@gmail.com>
2016-09-17 10:40:20 -0400

    first push

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+var config = {
+   apiKey: "AIzaSyBfnyqE8RL6yU1kKv0S2g_IAjBCwl68eF8",
+   authDomain: "employeesaturday.firebaseapp.com",
+   databaseURL: "https://employeesaturday.firebaseio.com",
+   storageBucket: "employeesaturday.appspot.com",
+   messagingSenderId: "1059478718374"
+ };
+ firebase.initializeApp(config);
